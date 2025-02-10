a5355874cac901f2402a07028808f4b9283bc732
arikarakas74 <ari.karakas@etu.sorbonne-universite.fr>
2025-02-10 23:55:02 +0100

    Deleted some lines and added functions for draw()

--- src/controller/simulation_controller.py
+++ src/controller/simulation_controller.py
@@ -2,17 +2,24 @@ from model.robot import Robot  # Adjusted the import path
 from view.control_panel import ControlPanel
 from controller.map_controller import MapController  # Import MapController
 from controller.robot_controller import RobotController  # Import RobotController
+from model.map_model import MapModel
+from view.robot_view import RobotView
 
 class SimulationController:
     """Handles the simulation flow and interactions between model and view."""
 
-    def __init__(self, map_instance, control_panel=None):
+    def __init__(self, map_instance, map_model, robot_view, control_panel=None):
+        self.map_model = map_model
+        self.robot_view = robot_view
         self.map = map_instance
         self.simulation_running = False
-        self.simulator = None  # Corrected placement
         self.robot_controller = None
         self.map_controller = MapController(self.map.map_model, self.map.map_view, self.map.window)  # Initialize map_controller with required arguments
         self.control_panel = control_panel  # Use the provided control_panel
+    
+    def update_view(self):
+        x, y, theta = self.map.robot.get_position()
+        self.map.robot_view.draw(x, y, theta)
 
     def run_simulation(self):
         """Starts the robot simulation."""
@@ -24,25 +31,17 @@ class SimulationController:
             self.map.map_view.update_message_label(text="Please set the start position.") # Access map_view
             return
 
-        self.map.robot = Robot(self.map.map_model.start_position, self.map.map_view.width, self.map.map_view.height)  # Create robot instance with map dimensions
+        self.map.robot = Robot(self.map.map_model)  # Create robot instance with map dimensions
+        self.update_view()
+        self.map.robot.add_event_listener(lambda event, **kwargs: self.update_view())
+        self.map.robot.start_movement()
         self.map.map_view.create_speed_label()  # Create speed label in the view
         self.control_panel.set_speed_label(self.map.map_view.speed_label)  # Set the speed label to control panel
         self.robot_controller = RobotController(self.map.robot, self.map.map_view.robot_view, self.control_panel, self.map.window)  # Pass window to RobotController
         self.map.map_view.update_message_label(text="Use W/S to move forward/backward and A/D to turn left/right.") # Access map_view
-        self.map.robot.draw()
-        self.map.robot.update_motion(self.map.map_model.obstacles, self.map.map_model.end_position) # Start motion update with required arguments
         self.simulation_running = True
 
     def reset_map(self, include_robot=False):
-        if self.simulator:
-            self.simulator.stop()
-            self.simulator = None
-
-        if self.map.robot:
-            if self.map.robot.current_after:
-                self.map.window.after_cancel(self.map.robot.current_after)
-            self.map.robot.stop()
-            self.map.robot = None
 
         self.simulation_running = False
         self.map.map_view.delete_all()  # Access map_view
