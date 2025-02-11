5c4cbd01d753624ddd359d10e63c0c776095bc6b
arikarakas74 <ari.karakas@etu.sorbonne-universite.fr>
2025-02-11 19:33:07 +0100

    Update draw function for a more accurate robot

--- src/view/robot_view.py
+++ src/view/robot_view.py
@@ -5,20 +5,32 @@ class RobotView:
         """Initializes the RobotView with a reference to the MapView."""
         self.map_view = map_view
 
-    def draw(self, x, y, direction_angle):
+    def draw(self, x, y, direction_angle, left_speed, right_speed):
         """Draws the robot on the canvas at the current position."""
         self.map_view.canvas.delete("robot")
-        cx, cy = round(x), round(y)
+        cx, cy = x, y
         size = 15
         angle = math.radians(direction_angle)
 
+        if left_speed == right_speed:
+            R = float('inf')
+            Cx, Cy = None, None
+        else:
+            # Dönüş yarıçapını hesapla
+            WHEEL_BASE_WIDTH = 10.0  # cm (İki tekerlek arası mesafe)
+            R = (WHEEL_BASE_WIDTH / 2) * (left_speed + right_speed) / (right_speed - left_speed)
+
+            # Dönüş merkezini hesapla
+            Cx = cx - R * math.sin(angle)
+            Cy = cy + R * math.cos(angle)
+
         # Calculate the points of the triangle representing the robot
-        p1 = (cx + size * math.cos(angle), cy + size * math.sin(angle))
-        p2 = (cx + size * math.cos(angle + 2.5), cy + size * math.sin(angle + 2.5))
-        p3 = (cx + size * math.cos(angle - 2.5), cy + size * math.sin(angle - 2.5))
+        front = (cx + size * math.cos(angle), cy + size * math.sin(angle))
+        left = (cx + size * math.cos(angle + 2.2), cy + size * math.sin(angle + 2.2))
+        right = (cx + size * math.cos(angle - 2.2), cy + size * math.sin(angle - 2.2))
 
         # Draw the robot as a blue triangle
-        self.map_view.canvas.create_polygon(p1, p2, p3, fill="blue", tags="robot")
+        self.map_view.canvas.create_polygon(front, left, right, fill="blue", tags="robot")
 
     def clear_robot(self):
         """Clears the robot from the canvas."""
