39c6cc47c3fad8902f57b0ff98f68217073c7914
chen-guanqi <cgq55308@outlook.com>
2025-03-17 12:48:53 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Robot Simulation MVC")
     parser.add_argument('mode',
-                        choices=['gui', 'cli'],
+                        choices=['gui', 'cli','vpython'],
                         nargs='?',
                         default='gui',
                         help="Execution mode: gui (default) or cli")
@@ -12,9 +12,12 @@ def main():
     if args.mode == 'gui':
         import gui_main
         gui_main.run_gui()
-    else:
+    elif args.mode == 'cli' :
         import cli_main
         cli_main.run_cli()
+    elif args.mode == 'vpython':
+        import vpython_main
+        vpython_main.run_vpython()
 
 if __name__ == "__main__":
     main()
